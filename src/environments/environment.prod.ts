ee14de05c1490dfbaf035cdd686e908b0a13f555
rasanga <rasanga95sa@gmail.com>
2022-05-08 20:21:02 +0530

    Deploy on firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCx6Ftc5BrIH7D10U6uYz3vGpN7FIsVKc8",
+    authDomain: "ezgarage-349506.firebaseapp.com",
+    projectId: "ezgarage-349506",
+    storageBucket: "ezgarage-349506.appspot.com",
+    messagingSenderId: "1035636604668",
+    appId: "1:1035636604668:web:bc4188c658dc2200d72179",
+    measurementId: "G-P05B84KKD6"
+  },
 };
